9cfba4c7cc8f6cbe7ae3eec6666970f5f8a65989
FelixTeutsch <felix@teutsch.it>
2024-01-23 14:01:54 +0100

    Very fancy stuff

---
+++ app/src/main/java/it/teutsch/felix/rant2text/ui/view/AboutView.kt
@@ -0,0 +1,12 @@
+package it.teutsch.felix.rant2text.ui.view
+
+import androidx.compose.runtime.Composable
+import it.teutsch.felix.rant2text.data.dataStore.SettingsData
+
+@Composable
+fun AboutView(
+    settings: SettingsData,
+    finish: () -> Unit
+) {
+
+}

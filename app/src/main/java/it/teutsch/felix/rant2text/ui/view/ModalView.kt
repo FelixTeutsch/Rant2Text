8034db9b0c4264512e477b843c4d111df59c6fb9
FelixTeutsch <felix@teutsch.it>
2024-01-24 09:47:59 +0100

    Anger Colour fixed

--- app/src/main/java/it/teutsch/felix/rant2text/ui/view/ModalView.kt
+++ app/src/main/java/it/teutsch/felix/rant2text/ui/view/ModalView.kt
@@ -233,12 +233,14 @@ fun CreateRantModal(
                             modifier = Modifier.fillMaxWidth()
                         )
 
+                        if (!isEditMode && settings != null) {
+                            rant.angerLevel =
+                                EAngerLevel.fromInt(settings.defaultAngerLevel.angerLevel)
+                        }
+
                         var rantLevel by rememberSaveable { mutableStateOf(rant.angerLevel) }
                         var rantValue by rememberSaveable { mutableFloatStateOf(rant.angerLevel.angerLevel.toFloat()) }
-                        if (!isEditMode && settings != null) {
-                            rantLevel = settings.defaultAngerLevel
-                            rantValue = settings.defaultAngerLevel.angerLevel.toFloat()
-                        }
+
                         Column(
                             modifier = Modifier.fillMaxWidth(),
                         ) {
@@ -266,6 +268,7 @@ fun CreateRantModal(
                             onConfirmColor = MaterialTheme.colorScheme.onPrimaryContainer,
                             cancelLabel = "Cancel",
                             onConfirm = {
+                                if (rantTitle.isEmpty()) return@ModalButtons
                                 rantViewModel.dismissDialog()
                                 if (isEditMode) {
                                     coroutineScope.launch {
